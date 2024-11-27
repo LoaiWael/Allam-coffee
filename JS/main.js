3935517bf982d5f196e22c542253acf1e4a08122
Loai Wael <princeloai70@gmail.com>
2024-11-27 02:01:52 +0200

    FIX issues

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@ $(function () {
     let isVisible = true
     $(".humburger").click(function (e) {
         if (isVisible) {
-            $("#menu").animate({ right: "146.45px" }, 500)
+            $("#menu").animate({ right: "146px" }, 500)
         } else {
             $("#menu").animate({ right: "0" }, 500)
         }
