64abba1fce4cb86cfecbe3b4647cb661ab57544d
Loai Wael <princeloai70@gmail.com>
2024-11-27 15:36:01 +0200

    ADD menu.html menu.js menu.css

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,9 @@ $(function () {
     let isVisible = true
     $(".humburger").click(function (e) {
         if (isVisible) {
-            $("#menu").animate({ right: "146px" }, 500)
+            $("#menu-nav").animate({ right: "146px" }, 500)
         } else {
-            $("#menu").animate({ right: "0" }, 500)
+            $("#menu-nav").animate({ right: "0" }, 500)
         }
         isVisible = !isVisible;
     });
