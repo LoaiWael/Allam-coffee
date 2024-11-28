c65a5437d462adfe55c97e19c69bfefeabfd48b1
Loai Wael <princeloai70@gmail.com>
2024-11-28 21:57:00 +0200

    UPD menu.css || menu.js || mnu.html

--- JS/menu.js
+++ JS/menu.js
@@ -10,9 +10,16 @@ $(function () {
         $(".nav-buttons-small").slideToggle(300);
     });
 
+    $(".fast-scroll-links").slideToggle();
+    $("#fast-scroll-button").click(function (e) {
+        $(".fast-scroll-links").css("visibility", "visible");
+        $(".fast-scroll-links").slideToggle("fast");
+    });
+
     const image = document.getElementsByTagName("img");
     image.setAttribute("draggable", "false");
+    image.setAttribute("onecontextmenu", "false");
     image.oncontextmenu = function () {
-        return false
+        return false;
     }
 })
